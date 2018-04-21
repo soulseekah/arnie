a937634a096ee8ea6d7f71bc1572027d40d77ab6
Shapito27 <legionerust@yandex.ru>
2018-04-21 20:21:03 +0300

    added bot frontend prototype

---
+++ js/arniebot.js
@@ -0,0 +1,40 @@
+(function ($) {
+    var arnieBot = {
+        currentBotId: null,
+        botWrapperClass: '.arniebot',
+        userMessageClass: '.client-board__message',
+        chatClass: '.arniebot__chat',
+        state: null,
+        init: function() {
+            this.currentBotId = $(this.botWrapperClass).data('bot-id');
+            this.events();
+        },
+        events: function () {
+            self = this;
+            $(document).on('click', '.client-board__message__send', function(e){
+                e.preventDefault();
+                self.send(self.currentBotId, $(self.userMessageClass).val());
+            });
+        },
+        send: function (botId, message) { // send message to bot
+            $(this.chatClass).append('<div class="arniebot__chat__message arniebot__chat__message--user">' + message  + '</div>');
+            var data = {'data': message, 'state':this.state};
+            self = this;
+            $.ajax({
+                type: 'POST',
+                url: '/wp-json/arnie/v1/bots/'+ botId,
+                dataType: 'json',
+                data: data,
+                success: function(data){
+                    self.state = data.state;
+                    $(self.chatClass).append('<div class="arniebot__chat__message arniebot__chat__message--bot">' + data.response  + '</div>');
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            });
+        }
+    };
+
+    arnieBot.init();
+}(jQuery));
